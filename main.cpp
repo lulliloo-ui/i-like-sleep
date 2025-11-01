a08f4e4425dfa2dded1232524c0294277ae0b07a
lulliloo-ui <madieva.tv@edu.spbstu.ru>
2025-11-01 23:53:49 +0300

    cw

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 
+ int ** convert(const int * t, size_t n, const size_t * lns, size_t rows)
+{
+  int ** mtx = nullptr;
+  **mtx = new * [rows];
+  for (int i = 0; i < rows; i ++)
+  {
+    int a = lns[i];
+    mtx[i] = new [a];
+  }
+}
 void destroy(int ** mtx, size_t created)
 {
   for (size_t i = 0; i < created; i++)
@@ -32,14 +42,35 @@ int ** construct(int ** mtx, int init, size_t rows, size_t cols)
 {
   for (size_t i = 0; i < rows; ++i)
   {
-    for (size_t j = 0; j < rows; ++j)
+    for (size_t j = 0; j < cols; ++j)
     {
       mtx[i][j] = init;
     }
   }
-  return mtx;
 }
 
+void input(int ** mtx, size_t rows, size_t cols)
+{
+  for (size_t i = 0; i < rows; ++i)
+  {
+    for (size_t j = 0; j < cols; ++j)
+    {
+      std::cin >> mtx[i][j];
+    }
+  }
+}
+void output(int ** mtx, size_t rows , size_t cols)
+{
+  for (size_t i = 0; i < rows; ++i)
+  {
+    std::cout << mtx[i][0];
+    for (size_t j = 1; j < cols; ++j)
+    {
+      std::cout << " " << mtx[i][j];
+    }
+    std::cout << "\n";
+  }
+}
 int main() {
   size_t r = 0, c = 0;
   std::cin >> r >> c;
@@ -47,7 +78,7 @@ int main() {
   {
     std::cerr << "plohoy input\n";
 
-    return 2;
+    return 1;
   }
   int ** matrix = nullptr;
   try
@@ -61,8 +92,14 @@ int main() {
     return 1;
   }
   std::cout << "created\n";
-  construct(matrix, 2, r, c);
-  std::cout << matrix[0][0] << '\n';
+  construct(matrix, 0, r, c);
+  input(matrix, r, c);
+  if (!std::cin)
+  {
+    destroy(matrix, r);
+    std::cerr << "input error\n";
+    return 1;
+  }
+  output(matrix, r, c);
   destroy(matrix, r);
 }
-
