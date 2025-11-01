485a838bad6c01a79a379bdec611d95b47d34f96
lulliloo-ui <madieva.tv@edu.spbstu.ru>
2025-11-01 13:42:05 +0300

    initialzng matrix

--- main.cpp
+++ main.cpp
@@ -28,17 +28,41 @@ int ** create(size_t rows, size_t cols)
   return mtx;
 }
 
+int ** construct(int ** mtx, int init, size_t rows, size_t cols)
+{
+  for (size_t i = 0; i < rows; ++i)
+  {
+    for (size_t j = 0; j < rows; ++j)
+    {
+      mtx[i][j] = init;
+    }
+  }
+  return mtx;
+}
+
 int main() {
+  size_t r = 0, c = 0;
+  std::cin >> r >> c;
+  if (!std::cin)
+  {
+    std::cerr << "plohoy input\n";
+
+    return 2;
+  }
   int ** matrix = nullptr;
   try
   {
-    matrix = create(5, 5);
+    matrix = create(r, c);
   }
   catch (const std::bad_alloc & e)
   {
     std::cerr << e.what() << '\n';
 
-  return 1;
+    return 1;
   }
+  std::cout << "created\n";
+  construct(matrix, 2, r, c);
+  std::cout << matrix[0][0] << '\n';
+  destroy(matrix, r);
 }
 
